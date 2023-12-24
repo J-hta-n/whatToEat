ff647f074bc73303b1c3170a0bb5d8d81de41af7
J-hta-n <e0969121@u.nus.edu>
2023-12-25 00:22:32 +0800

    Update database schema and zod validation

---
+++ prisma/migrations/20231224142910_remove_main_theme/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `main_theme` on the `FoodPlace` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "FoodPlace" DROP COLUMN "main_theme";
--- src/validationSchemas.ts
+++ src/validationSchemas.ts
@@ -1,25 +1,26 @@
+import { PlaceType, Region } from "@prisma/client";
 import { z } from "zod";
 
 export const addFoodPlaceSchema = z.object({
   place_name: z.string().min(1, "Name is required").max(255),
-  place_type: z.string(),
-  tried_before: z.coerce.boolean().default(false),
-  lb_cost: z.coerce.number().nullable().default(null).optional(),
-  ub_cost: z.coerce.number().nullable().default(null).optional(),
-  personal_rating: z.coerce.number().nullable().default(null).optional(),
-  google_rating: z.coerce.number().nullable().default(null).optional(),
-  region: z.string().optional(),
+  place_type: z.nativeEnum(PlaceType),
+  tried_before: z.coerce.boolean(),
+  lb_cost: z.coerce.number().nullable(),
+  ub_cost: z.coerce.number().nullable(),
+  personal_rating: z.coerce.number().nullable(),
+  google_rating: z.coerce.number().nullable(),
+  region: z.nativeEnum(Region),
 });
 
-export const initialFoodPlace = {
+export const defaultFoodPlace = {
   place_name: "",
-  place_type: "RESTAURANT",
+  place_type: PlaceType.RESTAURANT,
   tried_before: false,
-  lb_cost: undefined,
-  ub_cost: undefined,
-  personal_rating: undefined,
-  google_rating: undefined,
-  region: undefined,
+  lb_cost: null,
+  ub_cost: null,
+  personal_rating: null,
+  google_rating: null,
+  region: Region.ISLANDWIDE,
 };
 
 export type TAddFoodPlaceSchema = z.infer<typeof addFoodPlaceSchema>;
