48bd58b1e8d98409e113fda0bf665004a3d82638
J-hta-n <e0969121@u.nus.edu>
2023-12-26 01:28:55 +0800

    Update add food place form page

---
+++ src/app/foodplaces/_components/AddFoodPlaceDialog.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { Button, Dialog } from "@radix-ui/themes";
+import FoodPlaceForm from "./FoodPlaceForm";
+
+const AddFoodPlaceDialog = () => {
+  return (
+    <Dialog.Root>
+      <Dialog.Trigger>
+        <Button>Add new place</Button>
+      </Dialog.Trigger>
+      <Dialog.Content>
+        <Dialog.Title>New food place</Dialog.Title>
+        <FoodPlaceForm />
+      </Dialog.Content>
+    </Dialog.Root>
+  );
+};
+
+export default AddFoodPlaceDialog;
