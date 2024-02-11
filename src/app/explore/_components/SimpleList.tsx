0537338f552761b5591990bc6490a4cde42da04a
J-hta-n <e0969121@u.nus.edu>
2024-02-11 23:38:43 +0800

    Modify SimpleList

--- src/app/explore/_components/SimpleList.tsx
+++ src/app/explore/_components/SimpleList.tsx
@@ -1,13 +1,20 @@
 import { FoodPlace } from "@prisma/client";
+import { Text } from "@radix-ui/themes";
 import React from "react";
 
 interface Props {
   items: FoodPlace[] | null;
+  name: string;
 }
 
-const SimpleList = ({ items }: Props) => {
-  return (
-    <ul>{items?.map((item) => <li key={item.id}>{item.place_name}</li>)}</ul>
+const SimpleList = ({ items, name }: Props) => {
+  return items === null || items.length === 0 ? (
+    <Text className="text-center">Oops, no food places found for '{name}'</Text>
+  ) : (
+    <>
+      <Text className="text-center underline">{name}</Text>
+      <ul>{items?.map((item) => <li key={item.id}>{item.place_name}</li>)}</ul>
+    </>
   );
 };
 
