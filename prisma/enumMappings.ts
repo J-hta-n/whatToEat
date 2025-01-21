dc4dc406cfa0f8a146eeacb5aaa63678a7ee733f
J-hta-n <e0969121@u.nus.edu>
2025-01-21 18:18:00 +0800

    refactor: remove unused code

--- prisma/enumMappings.ts
+++ prisma/enumMappings.ts
@@ -1,21 +1,5 @@
 import { FoodPlace, PlaceType, Region } from "@prisma/client";
 
-// export enum PlaceType {
-//   RESTAURANT = "Restaurant",
-//   HAWKER_STALL = "Hawker stall",
-//   CANTEEN_STALL = "Canteen stall",
-//   BAKERY = "Bakery",
-//   DESSERT_PLACE = "Dessert place",
-// }
-
-// export enum Region {
-//   NORTH = "North",
-//   EAST = "East",
-//   CENTRAL = "Central",
-//   WEST = "West",
-//   ISLANDWIDE = "Islandwide",
-// }
-
 type EnumMapping = Record<PlaceType, string> | Record<Region, string>;
 
 export const enumMappings: Record<keyof FoodPlace, EnumMapping> = {
