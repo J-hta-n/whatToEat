215d48f061d916283061ebe629a8a73000c3e304
J-hta-n <e0969121@u.nus.edu>
2025-02-02 21:22:31 +0800

    feat(api): add support for foodplace taggings in PATCH api

--- src/app/api/foodplaces/[id]/route.ts
+++ src/app/api/foodplaces/[id]/route.ts
@@ -1,5 +1,11 @@
 import database from "@/prisma";
-import { TFoodPlaceSchema, foodPlaceSchema } from "@/validationSchemas";
+import {
+  TExploreArraysSchema,
+  TFoodPlaceByExploreArraysSchema,
+  TFoodPlaceSchema,
+  exploreArraysSchema,
+  foodPlaceSchema,
+} from "@/validationSchemas";
 import { NextRequest, NextResponse } from "next/server";
 
 interface Props {
@@ -7,7 +13,7 @@ interface Props {
 }
 
 export async function PATCH(req: NextRequest, { params }: Props) {
-  const body: TFoodPlaceSchema = await req.json();
+  const body: TFoodPlaceByExploreArraysSchema = await req.json();
   const validation = foodPlaceSchema.safeParse(body);
   if (!validation.success) {
     let zodErrors = {};
@@ -28,12 +34,134 @@ export async function PATCH(req: NextRequest, { params }: Props) {
         { status: 404 }
       );
     }
+    const updatedFoodPlaceBody: TFoodPlaceSchema = {} as TFoodPlaceSchema;
+    Object.keys(foodPlaceSchema.shape).forEach((key) => {
+      if (key in body) {
+        //@ts-ignore
+        updatedFoodPlaceBody[key] = body[key];
+      }
+    });
     const updatedFoodPlace = await database.foodPlace.update({
       where: { id: placeId },
       data: {
-        ...body,
+        ...updatedFoodPlaceBody,
       },
     });
+    const junctionTableArrays: TExploreArraysSchema =
+      {} as TExploreArraysSchema;
+    Object.keys(exploreArraysSchema.shape).forEach((key) => {
+      if (key in body) {
+        //@ts-ignore
+        junctionTableArrays[key] = body[key];
+      }
+    });
+
+    const oldCuisines = await database.foodPlaceByCuisine.findMany({
+      where: { place_id: placeId },
+    });
+    const oldDishes = await database.foodPlaceByDish.findMany({
+      where: { place_id: placeId },
+    });
+    const oldTags = await database.foodPlaceByTag.findMany({
+      where: { place_id: placeId },
+    });
+
+    const cuisinesByFoodPlaceToDelete = oldCuisines
+      .filter(
+        (cuisineByFoodPlace) =>
+          !junctionTableArrays["cuisines"]
+            .map((selectedCuisine) => selectedCuisine["value"])
+            .includes(cuisineByFoodPlace["cuisine_id"])
+      )
+      .map((cuisineByFoodPlace) => cuisineByFoodPlace["cuisine_id"]);
+    const cuisinesByFoodPlaceToInsert = junctionTableArrays["cuisines"]
+      .map((selectedCuisine) => selectedCuisine["value"])
+      .filter(
+        (id) =>
+          !oldCuisines
+            .map((cuisineByFoodPlace) => cuisineByFoodPlace["cuisine_id"])
+            .includes(id)
+      );
+
+    const dishesByFoodPlaceToDelete = oldDishes
+      .filter(
+        (dishByFoodPlace) =>
+          !junctionTableArrays["dishes"]
+            .map((selectedDish) => selectedDish["value"])
+            .includes(dishByFoodPlace["dish_id"])
+      )
+      .map((dishByFoodPlace) => dishByFoodPlace["dish_id"]);
+    const dishesByFoodPlaceToInsert = junctionTableArrays["dishes"]
+      .map((selectedDish) => selectedDish["value"])
+      .filter(
+        (id) =>
+          !oldDishes
+            .map((dishByFoodPlace) => dishByFoodPlace["dish_id"])
+            .includes(id)
+      );
+
+    const tagsByFoodPlaceToDelete = oldTags
+      .filter(
+        (tagByFoodPlace) =>
+          !junctionTableArrays["tags"]
+            .map((selectedTag) => selectedTag["value"])
+            .includes(tagByFoodPlace["tag_id"])
+      )
+      .map((tagByFoodPlace) => tagByFoodPlace["tag_id"]);
+    const tagsByFoodPlaceToInsert = junctionTableArrays["tags"]
+      .map((selectedTag) => selectedTag["value"])
+      .filter(
+        (id) =>
+          !oldTags
+            .map((tagByFoodPlace) => tagByFoodPlace["tag_id"])
+            .includes(id)
+      );
+
+    await database.foodPlaceByCuisine.deleteMany({
+      where: {
+        place_id: placeId,
+        cuisine_id: {
+          in: cuisinesByFoodPlaceToDelete,
+        },
+      },
+    });
+    await database.foodPlaceByCuisine.createMany({
+      data: cuisinesByFoodPlaceToInsert.map((id) => ({
+        place_id: placeId,
+        cuisine_id: id,
+      })),
+    });
+
+    await database.foodPlaceByDish.deleteMany({
+      where: {
+        place_id: placeId,
+        dish_id: {
+          in: dishesByFoodPlaceToDelete,
+        },
+      },
+    });
+    await database.foodPlaceByDish.createMany({
+      data: dishesByFoodPlaceToInsert.map((id) => ({
+        place_id: placeId,
+        dish_id: id,
+      })),
+    });
+
+    await database.foodPlaceByTag.deleteMany({
+      where: {
+        place_id: placeId,
+        tag_id: {
+          in: tagsByFoodPlaceToDelete,
+        },
+      },
+    });
+    await database.foodPlaceByTag.createMany({
+      data: tagsByFoodPlaceToInsert.map((id) => ({
+        place_id: placeId,
+        tag_id: id,
+      })),
+    });
+
     return NextResponse.json(updatedFoodPlace, { status: 200 });
   } catch (e) {
     console.log(e);
