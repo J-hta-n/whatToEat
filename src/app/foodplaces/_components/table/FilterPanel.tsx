6f8c0e327d424093dd7f0f3b10922da8cd54bff3
J-hta-n <e0969121@u.nus.edu>
2025-02-02 17:29:21 +0800

    feat: add explore page context to food place form

--- src/app/foodplaces/_components/table/FilterPanel.tsx
+++ src/app/foodplaces/_components/table/FilterPanel.tsx
@@ -2,18 +2,28 @@ import React from "react";
 import { FoodQuery } from "../../page";
 import { Flex } from "@radix-ui/themes";
 import FilterSelect from "./FilterSelect";
-import { PlaceType, Region } from "@prisma/client";
+import { Cuisine, Dish, PlaceType, Region, Tag } from "@prisma/client";
 import AddNewFoodPlaceDialog from "./AddNewFoodPlaceDialog";
+import database from "@/prisma";
 
 interface Props {
   searchParams: FoodQuery;
 }
 
-const FilterPanel = ({ searchParams }: Props) => {
+const FilterPanel = async ({ searchParams }: Props) => {
+  const cuisines: Cuisine[] = await database.cuisine.findMany();
+  const dishes: Dish[] = await database.dish.findMany();
+  const tags: Tag[] = await database.tag.findMany();
+  const explorePageContext = {
+    cuisines: cuisines,
+    dishes: dishes,
+    tags: tags,
+  };
+
   return (
     <Flex gap="5" className="m-5" align="center">
       <div className="mr-32">
-        <AddNewFoodPlaceDialog />
+        <AddNewFoodPlaceDialog explorePageContext={explorePageContext} />
       </div>
       <div className="w-1/5">
         <FilterSelect<PlaceType>
