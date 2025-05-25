1d08ed321dad54c829306203b800ab2566963715
J-hta-n <e0969121@u.nus.edu>
2025-05-25 16:27:30 +0800

    feat: stop scrolling back to top on shallow routing

--- src/app/_components/Pagination.tsx
+++ src/app/_components/Pagination.tsx
@@ -23,7 +23,7 @@ const Pagination = ({ curPage, totalPages }: Props) => {
   const goToPage = (page: number) => {
     const params = new URLSearchParams(searchParams);
     params.set("page", page.toString());
-    router.push("?" + params.toString());
+    router.push(`?${params.toString()}`, { scroll: false });
   };
 
   return (
